b943d8434b8e07ec72c31d6507f5449a19bb2585
amaagomez <agomezdisanto@udesa.edu.ar>
2025-09-14 14:15:10 -0300

    pagina NotFound

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import Home from './screens/Home/home';
 import detalle from './screens/Detalle/detale';
+import NotFound from './screens/NotFound/NotFound'
 
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
      <Switch>
     <Route component = {Home} path = '/' exact = {true} />
     <Route component = {detalle} path = '/detalle/:id' exact = {true} />
+    
 
 
-
+    <Route component = {NotFound} path = '' />
 
      </Switch>
 
---
+++ src/screens/NotFound/NotFound.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import Header from "../../components/Header/header";
+import Card from "../../components/Card/card";
+import ListaCards from "../../components/ListaCards/listacards";
+
+class NotFound extends Component{
+constructor (props){
+    super(props)
+    this.state = {
+       
+    };
+}
+
+
+
+render (){
+    return(
+        <p>No existe esa ruta</p>
+    )
+}
+
+}
+
+export default NotFound
